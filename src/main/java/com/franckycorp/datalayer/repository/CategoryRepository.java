0e82339d6a2244d99246b8f81225ae7c136c9857
FranckPedron <franckpedrondev@gmail.com>
2022-12-29 03:51:21 +0100

    Build - add entities

---
+++ src/main/java/com/franckycorp/datalayer/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.franckycorp.datalayer.repository;
+
+import com.franckycorp.datalayer.model.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+}
---
+++ src/main/java/com/franckycorp/datalayer/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.franckycorp.datalayer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
